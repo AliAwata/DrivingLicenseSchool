81b564014121fcdc0cac88dad6093c0a286f4fa3
Ali Awata <edgylordoptimus@gmail.com>
2022-06-22 18:18:53 +0300

    commit

---
+++ DrivingSclApp/Areas/Indexes/Controllers/zCategoryController.cs
@@ -0,0 +1,139 @@
+﻿using DrivingSclApp.Areas.Indexes.Data;
+using DrivingSclData;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace DrivingSclApp.Areas.Indexes.Controllers
+{
+    [Authorize, Audit, CanDoIt, NoCache, RedirectOnError]
+    public class zCategoryController : Controller
+    {
+        private DrivingSclEntity db = new DrivingSclEntity();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Read([DataSourceRequest] DataSourceRequest request)
+        {
+
+            List<CategoryVM> Data = new List<CategoryVM>();
+            Data = (from c in db.ZCATEGORY
+                    select new CategoryVM{
+                        NB = c.NB,
+                        NAME = c.NAME,
+                        PREV_CATG = c.PREV_CATG,
+                        STUDENT_PER_CAR = c.STUDENT_PER_CAR,
+                        STUDENT_PER_MOTO = c.STUDENT_PER_MOTO,
+                        METERS_FOR_STUDENT = c.METERS_FOR_STUDENT,
+                        MAX_STUDENTS_PER_HALL = c.MAX_STUDENTS_PER_HALL,
+                        NOTE = c.NOTE,
+                    }).ToList();
+
+            foreach(var item in Data)
+            {
+                item.CATG_NAME = getCategory(item.NB);
+            }
+
+            return Json(Data.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+        public string getCategory(long id)
+        {
+            string prev_category = "";
+            var Data = db.ZCATEGORY.Find(id);
+            var category = db.ZCATEGORY.Find(Data.PREV_CATG);
+            
+            if(category != null)
+            {
+                prev_category = category.NAME;
+            }
+
+            return prev_category;
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create([DataSourceRequest] DataSourceRequest request, ZCATEGORY model)
+        {
+            using (var transaction = db.Database.BeginTransaction())
+            {
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        model.NB = MyDataBase.GetSeqValue("GetIndexID");
+                        db.ZCATEGORY.Add(model);
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        return Json(new { success = false, responseText = ex.InnerException.InnerException.Message }, JsonRequestBehavior.AllowGet);
+                    }
+                }
+                else
+                    return Json(new { success = false, responseText = "الرجاء التأكد من المدخلات!" }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { success = true, responseText = "تمت الإضافة بنجاح" }, JsonRequestBehavior.AllowGet);
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Update([DataSourceRequest] DataSourceRequest request, ZCATEGORY model)
+        {
+            using (var transaction = db.Database.BeginTransaction())
+            {
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        db.ZCATEGORY.Attach(model);
+                        db.Entry(model).State = EntityState.Modified;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        return Json(new { success = false, responseText = ex.InnerException.InnerException.Message }, JsonRequestBehavior.AllowGet);
+                    }
+                }
+                else
+                    return Json(new { success = false, responseText = "الرجاء التأكد من المدخلات!" }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { success = true, responseText = "تم التعديل بنجاح" }, JsonRequestBehavior.AllowGet);
+
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Destroy([DataSourceRequest] DataSourceRequest request, ZCATEGORY model)
+        {
+            using (var transaction = db.Database.BeginTransaction())
+            {
+                try
+                {
+                    db.ZCATEGORY.Attach(model);
+                    db.Entry(model).State = EntityState.Deleted;
+                    db.SaveChanges();
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    return Json(new { success = false, responseText = ex.InnerException.InnerException.Message }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            return Json(new { success = true, responseText = "تم الحذف بنجاح" }, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult validation(string param1)
+        {
+            string ss = MyDataBase.RunQuery("select count(*) from ZCATEGORY where NAME='" + param1 + "'");
+            if (Convert.ToInt32(ss) >= 1)
+            {
+                return Json(new { success = false, responseText = "الاسم موجود بالفعل" }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { success = true, responseText = "تمت عملية الحفظ بنجاح" }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
